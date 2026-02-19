e2aa477a033df37832575e0e78c91acdf3881e43
Rajvenkadam <amux525@gmail.com>
2026-02-19 21:50:56 +0530

    Add new Java files

---
+++ BeginnerLevel/inttodouble.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class inttodouble {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        double d = num;
+        System.out.println(d);
+        sc.close();
+    }
+}
